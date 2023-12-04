3c5d6dfd0508c6a36ec7d8d98a7cfe3b6660e9b9
WanderJ08 <wanderroque01@gmail.com>
2023-12-04 14:28:40 -0400

    "cambios del register realizados"

--- models/Usuario.js
+++ models/Usuario.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const usuarioSchema = mongoose.Schema({
   nombre: {
     type: String,
-    required: true,
+    required: false,
   },
   apellido: {
     type: String,
-    required: true,
+    required: false,
   },
   email: {
     type: String,
